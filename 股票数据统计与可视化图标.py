2420559e9c5c7474e38fdf5cb01d81967a2fb7db
qunge <qunge1@outlook.com>
2019-01-17 17:33:19 +0800

    股票统计分析，未写完

--- 股票数据统计与可视化图标.py
+++ 股票数据统计与可视化图标.py
@@ -1,3 +1,4 @@
+"""
 # 1. 学生成绩单生成
 # 题目要求：一个班级20人，生成一份学号与随机数组成绩单，并写入文件
 import random
@@ -48,3 +49,65 @@ def megreScore(fname, source1, source2):
 megreScore('result.txt', './chinese.txt', './math.txt')
 
 # 2.2 新的需求来了
+"""
+"""
+1. 课程数量不确认，可能三科，可能四科
+2. 如果有人缺考，学号对应的成绩不存在
+一个班级20个人但某科目可能只有19条成绩
+"""
+"""
+
+
+# 合并两个成绩到一个文件
+# output:保存结果文件
+# nums：班级人数
+# subjects：成绩单文件名，长度不固定
+def megreMoreScores(output, nums, *subjects):
+    # 保存成绩单
+    list_subject = []
+    fw = open(output, 'w')
+    # 待处理每个成绩单
+    for fname in subjects:
+        f = open(fname)
+        # 保留每条成绩
+        info = []
+        for line in f:
+            # 数据格式：[[学号，成绩]]
+            info.append(line.split())
+        dinfo = dict(info)
+        # 每个科目成绩添加到列表：[{},{}]
+        list_subject.append(dict(info))
+        f.close()
+    # 按学号获取成绩
+    for i in range(1, nums + 1):
+        # 整理一个学生对应成绩
+        line = []
+        line.append(str(i))  # 添加学号
+        # 添加每个科目分数
+        for data in list_subject:
+            # 如果缺考默认值为0
+            line.append(str(' ' + data.get(str(i), 0)) + ' ')
+        # 写入数据
+        wline = ''.join(line) + '\n'
+        fw.write(wline)
+    fw.close()
+
+
+# 为了看到调试结果，这里添加三个文件，一个数学成绩（将一个学号对应成绩去掉），两个语文成绩
+megreMoreScores('tresult2.txt', 20, './math.txt', './chinese.txt', './chinese.txt')
+
+
+
+"""
+
+# 3. 股票统计分析
+"""
+A股2018最后一个交易日的部分数据，下载地址：https://pan.baidu.com/s/1vKDE18Cgw8ZaTMJQepDdZQ
+3.1 价格区间统计
+"""
+
+
+# 生成价格与价格区间，价格区间与数量字典
+def genPriceRange(price_range):
+    # 区间：数量字典
+    dresult=dict.fromkeys(price_range,0)
